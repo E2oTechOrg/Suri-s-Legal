c480bcd5a736da204c49e3596a50efd337f5d828
Hemapriya-e2o <hemapriya@e2o.in>
2025-12-31 17:32:52 +0530

    working on home page, description upload

--- js/main.js
+++ js/main.js
@@ -44,7 +44,6 @@
         $(window).resize(toggleNavbarMethod);
     });
 
-
     // Testimonials carousel
     $(".testimonials-carousel").owlCarousel({
         autoplay: true,
@@ -104,3 +103,14 @@
     
 })(jQuery);
 
+fetch("header.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("header").innerHTML = data;
+  });
+
+  fetch("footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("footer").innerHTML = data;
+  });
